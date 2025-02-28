29926da89f98bd9fffa101f25fc0796b6a2ac49e
kyo.loan <kyo.loan@freetrend.com.vn>
2025-02-28 10:21:53 +0700

    commit

--- pages/views/Function/sendMessageFB.tsx
+++ pages/views/Function/sendMessageFB.tsx
@@ -1,26 +1,30 @@
 import axios from "axios";
 
-export default async function sendMessageToUser(userID, messageText, pageAccessToken) {
+export default function sendMessageToUser(userID, messageText, pageAccessToken) {
     try {
         console.log("adadadad", userID, messageText, pageAccessToken)
         if (typeof window !== 'undefined') {
             const url = "https://graph.facebook.com/v22.0/122147223470399846/messages?access_token=" + pageAccessToken;
-            const response = await axios.post(url,
-                {
-                    recipient: { id: userID },
-                    messaging_type: "RESPONSE",
-                    message: { text: messageText }
-                },
-                {
-                    headers: {
-                        'Content-Type': "application/json"
-                    }
-                }
-            )
+            const response = apiSendMessage(url, userID, messageText)
             return response
         }
     } catch (error) {
         console.error(error)
     }
 
+}
+
+const apiSendMessage = async (url, userID, messageText) => {
+    return await axios.post(url,
+        {
+            recipient: { id: userID },
+            messaging_type: "RESPONSE",
+            message: { text: messageText }
+        },
+        {
+            headers: {
+                'Content-Type': "application/json"
+            }
+        }
+    )
 }
